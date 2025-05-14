2c30bf38eb71ab01e21da59794589a2eb4d19cf9
ac7x <ac7x@pm.me>
2025-05-14 12:27:02 +0000

    feat: 新增 GlobalBottomNav 組件並更新客戶端頁面結構

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { GlobalBottomNav } from '@/modules/shared/interfaces/navigation/GlobalBottomNav';
 
 const ClientPage: React.FC = () => {
   return (
-    <div>
+    <div className="pb-16">
       <h1>Client Page</h1>
       <p>這是客戶端頁面的基礎結構。</p>
+      <GlobalBottomNav />
     </div>
   );
 };
