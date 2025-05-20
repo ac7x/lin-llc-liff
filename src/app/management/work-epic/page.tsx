4a8677a83b89ffb7b06d537c3c6a5780b910939e
ac7x <ac7x@pm.me>
2025-05-21 01:50:33 +0800

    更新 page.tsx

--- src/app/management/work-epic/page.tsx
+++ src/app/management/work-epic/page.tsx
@@ -342,7 +342,7 @@ export default function WorkEpicPage() {
                     </tbody>
                 </table>
             </main>
-            <ClientBottomNav />
+            <ManagementBottomNav />
         </>
     );
 }
