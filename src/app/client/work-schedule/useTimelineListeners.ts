6e42cb4e46ccf940cfe9bcce30d4c2207b132ff7
ac7x <ac7x@pm.me>
2025-05-23 23:54:03 +0800

    feat: 更新 useTimelineListeners 的事件處理器類型，增強類型安全性與可擴展性

--- src/app/client/work-schedule/useTimelineListeners.ts
+++ src/app/client/work-schedule/useTimelineListeners.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { Timeline, TimelineItem } from 'vis-timeline/standalone'
 
+// 事件參數型別
 export interface AddEventProps {
     item: TimelineItem
     callback: (item: TimelineItem | null) => void
@@ -18,41 +19,100 @@ export type TimelineEventHandler<T = unknown> = (props: T) => void
 
 export interface UseTimelineListenersProps {
     timeline: Timeline | null
-    onSelect?: TimelineEventHandler
-    onItemOver?: TimelineEventHandler
-    onItemOut?: TimelineEventHandler
-    onRangeChange?: TimelineEventHandler
-    onRangeChanged?: TimelineEventHandler
-    onClick?: TimelineEventHandler
-    onDoubleClick?: TimelineEventHandler
-    onContextMenu?: TimelineEventHandler
-    onDrop?: TimelineEventHandler
+    onSelect?: TimelineEventHandler<any>
+    onDeselect?: TimelineEventHandler<any>
+    onItemOver?: TimelineEventHandler<any>
+    onItemOut?: TimelineEventHandler<any>
+    onItemSelected?: TimelineEventHandler<any>
+    onItemUnselected?: TimelineEventHandler<any>
+    onRangeChange?: TimelineEventHandler<any>
+    onRangeChanged?: TimelineEventHandler<any>
+    onClick?: TimelineEventHandler<any>
+    onDoubleClick?: TimelineEventHandler<any>
+    onContextMenu?: TimelineEventHandler<any>
+    onBackgroundClick?: TimelineEventHandler<any>
+    onBackgroundDoubleClick?: TimelineEventHandler<any>
+    onBackgroundContextMenu?: TimelineEventHandler<any>
+    onDrop?: TimelineEventHandler<any>
     onAdd?: TimelineEventHandler<AddEventProps>
     onMove?: TimelineEventHandler<MoveEventProps>
-    onRemove?: TimelineEventHandler
-    onStartResizing?: TimelineEventHandler
-    onEndResizing?: TimelineEventHandler
+    onRemove?: TimelineEventHandler<any>
+    onItemUpdate?: TimelineEventHandler<any>
+    onStartResizing?: TimelineEventHandler<any>
+    onEndResizing?: TimelineEventHandler<any>
+    onChanged?: TimelineEventHandler<any>
+    onMouseOver?: TimelineEventHandler<any>
+    onMouseDown?: TimelineEventHandler<any>
+    onMouseUp?: TimelineEventHandler<any>
+    onMouseMove?: TimelineEventHandler<any>
+    onMouseOut?: TimelineEventHandler<any>
+    onGroupDragged?: TimelineEventHandler<any>
+    onGroupDraggedEnd?: TimelineEventHandler<any>
+    onMarkerChange?: TimelineEventHandler<any>
+    onCurrentTimeTick?: TimelineEventHandler<any>
+    onTimeChanged?: TimelineEventHandler<any>
+    onDestroy?: TimelineEventHandler<any>
     // ...持續擴充
 }
 
+// 事件對應表
+const EVENT_PROP_MAP = [
+    { event: 'select', prop: 'onSelect' },
+    { event: 'deselect', prop: 'onDeselect' },
+    { event: 'itemover', prop: 'onItemOver' },
+    { event: 'itemout', prop: 'onItemOut' },
+    { event: 'itemselected', prop: 'onItemSelected' },
+    { event: 'itemunselected', prop: 'onItemUnselected' },
+    { event: 'rangechange', prop: 'onRangeChange' },
+    { event: 'rangechanged', prop: 'onRangeChanged' },
+    { event: 'click', prop: 'onClick' },
+    { event: 'doubleClick', prop: 'onDoubleClick' },
+    { event: 'contextmenu', prop: 'onContextMenu' },
+    { event: 'backgroundClick', prop: 'onBackgroundClick' },
+    { event: 'backgroundDoubleClick', prop: 'onBackgroundDoubleClick' },
+    { event: 'backgroundContextMenu', prop: 'onBackgroundContextMenu' },
+    { event: 'drop', prop: 'onDrop' },
+    { event: 'add', prop: 'onAdd' },
+    { event: 'move', prop: 'onMove' },
+    { event: 'remove', prop: 'onRemove' },
+    { event: 'itemupdate', prop: 'onItemUpdate' },
+    { event: 'startResizing', prop: 'onStartResizing' },
+    { event: 'endResizing', prop: 'onEndResizing' },
+    { event: 'changed', prop: 'onChanged' },
+    { event: 'mouseOver', prop: 'onMouseOver' },
+    { event: 'mouseDown', prop: 'onMouseDown' },
+    { event: 'mouseUp', prop: 'onMouseUp' },
+    { event: 'mouseMove', prop: 'onMouseMove' },
+    { event: 'mouseOut', prop: 'onMouseOut' },
+    { event: 'groupDragged', prop: 'onGroupDragged' },
+    { event: 'groupDraggedEnd', prop: 'onGroupDraggedEnd' },
+    { event: 'markerChange', prop: 'onMarkerChange' },
+    { event: 'currentTimeTick', prop: 'onCurrentTimeTick' },
+    { event: 'timechanged', prop: 'onTimeChanged' },
+    { event: 'destroy', prop: 'onDestroy' },
+] as const
+
 export const useTimelineListeners = (props: UseTimelineListenersProps): void => {
-    const { timeline, ...listeners } = props
+    const { timeline } = props
 
     useEffect(() => {
         if (!timeline) return
 
-        Object.entries(listeners).forEach(([event, handler]) => {
+        EVENT_PROP_MAP.forEach(({ event, prop }) => {
+            const handler = props[prop as keyof UseTimelineListenersProps]
             if (handler) {
-                timeline.on(event.replace(/^on/, '').toLowerCase(), handler)
+                timeline.on(event, handler as any)
             }
         })
 
         return () => {
-            Object.entries(listeners).forEach(([event, handler]) => {
+            EVENT_PROP_MAP.forEach(({ event, prop }) => {
+                const handler = props[prop as keyof UseTimelineListenersProps]
                 if (handler) {
-                    timeline.off(event.replace(/^on/, '').toLowerCase(), handler)
+                    timeline.off(event, handler as any)
                 }
             })
         }
-    }, [timeline, listeners])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [timeline, ...EVENT_PROP_MAP.map(({ prop }) => props[prop as keyof UseTimelineListenersProps])])
 }
