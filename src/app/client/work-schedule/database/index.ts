d33ef6d07e3242878977bfb1d913f2baa75df7ed
ac7x <ac7x@pm.me>
2025-05-24 00:52:27 +0800

    feat: 新增資料庫匯出檔案，整合 Redis、Firebase 及 Firebase Admin 客戶端

---
+++ src/app/client/work-schedule/database/index.ts
@@ -0,0 +1,15 @@
+// Redis 相關匯出
+export { redisCache } from './redis.client'
+
+// Firebase 相關匯出
+export {
+    firebaseApp,
+    firestore
+} from './firebase.client'
+
+// Firebase Admin 相關匯出
+export {
+    FirebaseAdminClient,
+    firebaseAdminClient,
+    firestoreAdmin
+} from './firebase-admin.client'
